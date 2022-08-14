a8feb6c9c55d9758ca48df3c650818de1863a94d
Pradip Khatri <p_khatri168959@fanshaweonline.ca>
2022-08-14 03:18:22 -0400

    trial

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ const mydatabase = new Mydatabase();
 if ('serviceWorker' in navigator) {
   console.log("hello service worker");
   navigator.serviceWorker
-    .register('/unsentest/service-worker.js', {scope:'/unsentest/', type:'module'})
+    .register('/service-worker.js', {scope:'/unsentest', type:'module'})
     .catch(function (error) {
       console.log('Service Worker failed to register:', error);
     });
@@ -140,15 +140,15 @@ if (navigator?.storage?.estimate){
   });
 }
 
-window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
-// DON'T use "var indexedDB = …" if you're not in a function.
-// Moreover, you may need references to some window.IDB* objects:
-window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || {READ_WRITE: "readwrite"}; // This line should only be needed if it is needed to support the object's constants for older browsers
-window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
-// (Mozilla has never prefixed these objects, so we don't need window.mozIDB*)
-if (!window.indexedDB) {
-  console.log("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
-}
+// window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+// // DON'T use "var indexedDB = …" if you're not in a function.
+// // Moreover, you may need references to some window.IDB* objects:
+// window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || {READ_WRITE: "readwrite"}; // This line should only be needed if it is needed to support the object's constants for older browsers
+// window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+// // (Mozilla has never prefixed these objects, so we don't need window.mozIDB*)
+// if (!window.indexedDB) {
+//   console.log("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
+// }
 
 
 
