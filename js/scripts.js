ef5b580c42ad89587a658c7fa69295c4a0fa21cf
Pradip Khatri <p_khatri168959@fanshaweonline.ca>
2022-08-14 03:05:42 -0400

    te

--- js/scripts.js
+++ js/scripts.js
@@ -140,15 +140,15 @@ if (navigator?.storage?.estimate){
   });
 }
 
-// window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
-// // DON'T use "var indexedDB = …" if you're not in a function.
-// // Moreover, you may need references to some window.IDB* objects:
-// window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || {READ_WRITE: "readwrite"}; // This line should only be needed if it is needed to support the object's constants for older browsers
-// window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
-// // (Mozilla has never prefixed these objects, so we don't need window.mozIDB*)
-// if (!window.indexedDB) {
-//   console.log("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
-// }
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+// DON'T use "var indexedDB = …" if you're not in a function.
+// Moreover, you may need references to some window.IDB* objects:
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || {READ_WRITE: "readwrite"}; // This line should only be needed if it is needed to support the object's constants for older browsers
+window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+// (Mozilla has never prefixed these objects, so we don't need window.mozIDB*)
+if (!window.indexedDB) {
+  console.log("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
+}
 
 
 
